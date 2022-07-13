3fe166819c42d32286e8306eebbbf3ee0d121dfd
kjw6121 <technet4111@gmail.com>
2022-07-13 15:48:54 +0900

    php11

--- firstpage.php
+++ firstpage.php
@@ -1,60 +1,45 @@
 <?php
-$serverName = "technetdb.czo9ia0scsp3.ap-northeast-2.rds.amazonaws.com";
-$connectionOptions = array(
-    "database" => "technetdbver2",
-    "uid" => "kim_jinwoo",
+    // Establishes the connection
+    $conn = sqlsrv_connect("technetdb.czo9ia0scsp3.ap-northeast-2.rds.amazonaws.com", "database" => "technetdbver2", "uid" => "kim_jinwoo",
     "pwd" => "technet4111",
-    "TrustServerCertificate" => "yes",
-);
+    "TrustServerCertificate" => "yes");
+    if ($conn === false) {
+        die(formatErrors(sqlsrv_errors()));
+    }
 
-function exception_handler($exception) {
-    echo "<h1>Failure</h1>";
-    echo "Uncaught exception: " , $exception->getMessage();
-    echo "<h1>PHP Info for troubleshooting</h1>";
-    phpinfo();
-}
-
-set_exception_handler('exception_handler');
-
-// Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if ($conn === false) {
-    die(formatErrors(sqlsrv_errors()));
-}
-
-// Select Query
-$tsql = "SELECT SN, 품번, 수량  FROM BHsch74 WHERE SN=100000";
+    // Select Query
+    $tsql = "SELECT SN, 품번, 수량  FROM BHsch74 WHERE SN=100000";
 
 
-// Executes the query
-$stmt = sqlsrv_query($conn, $tsql);
+    // Executes the query
+    $stmt = sqlsrv_query($conn, $tsql);
 
-// Error handling
-if ($stmt === false) {
-    die(formatErrors(sqlsrv_errors()));
-}
+    // Error handling
+    if ($stmt === false) {
+        die(formatErrors(sqlsrv_errors()));
+    }
 ?>
 
 <h1> Success Results : </h1>
 
 <?php
-while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-    echo $row['SN'] . $row['품번'] . $row['수량'] . PHP_EOL;
-}
-
-sqlsrv_free_stmt($stmt);
-sqlsrv_close($conn);
-
-function formatErrors($errors)
-{
-    // Display errors
-    echo "<h1>SQL Error:</h1>";
-    echo "Error information: <br/>";
-    foreach ($errors as $error) {
-        echo "SQLSTATE: ". $error['SQLSTATE'] . "<br/>";
-        echo "Code: ". $error['code'] . "<br/>";
-        echo "Message: ". $error['message'] . "<br/>";
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        echo $row['SN'] . $row['품번'] . $row['수량'] . PHP_EOL;
     }
-}
+
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn);
+
+    function formatErrors($errors)
+    {
+        // Display errors
+        echo "<h1>SQL Error:</h1>";
+        echo "Error information: <br/>";
+        foreach ($errors as $error) {
+            echo "SQLSTATE: ". $error['SQLSTATE'] . "<br/>";
+            echo "Code: ". $error['code'] . "<br/>";
+            echo "Message: ". $error['message'] . "<br/>";
+        }
+    }
 ?>
                   
