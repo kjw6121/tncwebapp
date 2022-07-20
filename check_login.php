ffa2abb181da7917af69e3e24d8f32f935f8b3da
kjw6121 <technet4111@gmail.com>
2022-07-20 12:11:24 +0900

    TTT

--- check_login.php
+++ check_login.php
@@ -40,7 +40,7 @@
       //$q = "SELECT * FROM member WHERE id = '$username' AND pass = '$userpass'";
       
     // Select Query
-    $tsql = "SELECT 사용자ID, pw FROM 담당자 WHERE 사용자ID = '$username' AND pw = '$userpass'";
+    $tsql = "SELECT * FROM 담당자 WHERE 사용자ID = '$username' AND pw = '$userpass'";
 
 
     // Executes the query
@@ -50,7 +50,7 @@
       //결과가 존재하면 세션 생성
       if ($row != null) {
          $_SESSION['username'] = $row['사용자ID'];
-         $_SESSION['name'] = $row['pw'];
+         $_SESSION['name'] = $row['성명'];
          echo "<script>location.replace('index.php');</script>";
          exit;
       }
