5348c0763f16660a1cc897ecb8ed3f87883d07f9
kjw6121 <technet4111@gmail.com>
2022-07-20 12:18:11 +0900

    TRR

--- check_login.php
+++ check_login.php
@@ -40,26 +40,15 @@
       //$q = "SELECT * FROM member WHERE id = '$username' AND pass = '$userpass'";
       
     // Select Query
-    $tsql = "SELECT * FROM 담당자 WHERE 사용자ID = '$username' AND pw = '$userpass'";
+    $tsql = "SELECT 사용자ID, 성명 FROM 담당자 WHERE 사용자ID = '$username' AND pw = '$userpass'";
 
 
     // Executes the query
     $stmt = sqlsrv_query($conn, $tsql);
     $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
       
-      //결과가 존재하면 세션 생성
-      if ($row != null) {
-         $_SESSION['username'] = $row['사용자ID'];
-         $_SESSION['name'] = $row['성명'];
-         echo "<script>location.replace('index.php');</script>";
-         exit;
-      }
-      
-      //결과가 존재하지 않으면 로그인 실패
-      if($row == null){
-         echo "<script>alert('Invalid username or password')</script>";
-         echo "<script>location.replace('login.php');</script>";
-         exit;
-      }
-      ?>
-   </body>
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+      echo '<tr><td>' . $row['사용자ID']. '</td>' .
+          '<td>' . $row['성명']. '</td>''</tr>';
+  }
+  ?>
