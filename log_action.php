1b145bbf33d6d7b8d530e4e0a6315deabb6cae41
kjw6121 <technet4111@gmail.com>
2022-07-21 10:43:50 +0900

    h

--- log_action.php
+++ log_action.php
@@ -1,6 +1,7 @@
 <?php
-session_start();
-$serverName = "technetdb.czo9ia0scsp3.ap-northeast-2.rds.amazonaws.com";
+ function OpenConnection()
+ {
+    $serverName = "technetdb.czo9ia0scsp3.ap-northeast-2.rds.amazonaws.com";
 $connectionInfo = arrayarray(
     "database" => "technetdbver2",
     "uid" => "kim_jinwoo",
@@ -8,22 +9,52 @@ $connectionInfo = arrayarray(
     "TrustServerCertificate" => "yes",
 );
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
-if( $conn === false){
-    echo "Error in connection.\n";
-    die( print_r( sqlsrv_errors(), true));
-}
-$username = $_REQUEST['uNm'];
-$password  = $_REQUEST['uPw'];
-$tsql = "SELECT * FROM 담당자 WHERE 사용자ID='$username' AND pw='$password'";
-$stmt = sqlsrv_query( $conn, $tsql);
-if(!sqlsrv_fetch($stmt)){
-    $_SESSION['valid_user'] = true;
-    $_SESSION['uNm'] = $username;
-    header('Location: index.php');
-    die();
-}else{
-    header('Location: error.html');
-    die();
-}
+     if($conn == false)
+         die(FormatErrors(sqlsrv_errors()));
 
+     return $conn;
+ }
+?>
+
+<?php
+function ReadData()
+{
+    try
+    {
+        $conn = OpenConnection();
+        $tsql = "SSELECT 성명 FROM 담당자 WHERE 사용자ID='$username' AND pw='$password'";
+        $getProducts = sqlsrv_query($conn, $tsql);
+        if ($getProducts == FALSE)
+            die(FormatErrors(sqlsrv_errors()));
+        $productCount = 0;
+        while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+        {
+            echo($row['성명']);
+            echo("<br/>");
+            $productCount++;
+        }
+        sqlsrv_free_stmt($getProducts);
+        sqlsrv_close($conn);
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
+}
+?>
+
+<?php
+//$username = $_REQUEST['uNm'];
+//$password  = $_REQUEST['uPw'];
+//$tsql = "SELECT * FROM 담당자 WHERE 사용자ID='$username' AND pw='$password'";
+//$stmt = sqlsrv_query( $conn, $tsql);
+//if(!sqlsrv_fetch($stmt)){
+   // $_SESSION['valid_user'] = true;
+   // $_SESSION['uNm'] = $username;
+   // header('Location: index.php');
+   // die();
+//}else{
+  //  header('Location: error.html');
+   // die();
+//}
 ?>
