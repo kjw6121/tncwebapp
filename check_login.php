6f0e1efdfefe542646e5667832674105d710ee12
kjw6121 <technet4111@gmail.com>
2022-07-21 12:03:40 +0900

    vbnb

--- check_login.php
+++ check_login.php
@@ -29,12 +29,9 @@
       
       $tsql = "SELECT 사용자ID, 성명 FROM 담당자 WHERE 사용자ID = '$username' AND pw = '$userpass'";
       $stmt = sqlsrv_query($conn, $tsql);
-      $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-  
-
-      
+        
       //결과가 존재하면 세션 생성
-      if ($row != null) {
+      if ($stmt != null) {
          $_SESSION['username'] = $row['사용자ID'];
          $_SESSION['name'] = $row['성명'];
          echo "<script>location.replace('index.php');</script>";
@@ -42,7 +39,7 @@
       }
       
       //결과가 존재하지 않으면 로그인 실패
-      if($row == null){
+      if($stmt == null){
          echo "<script>alert('Invalid username or password')</script>";
          echo "<script>location.replace('login.php');</script>";
          exit;
