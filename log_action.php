b8b396c60fc283e416a52e2a75504062d22af954
kjw6121 <technet4111@gmail.com>
2022-07-21 11:23:35 +0900

    ㅛ

--- log_action.php
+++ log_action.php
@@ -1,6 +1,6 @@
 <?php
- function OpenConnection()
- {
+function OpenConnection()
+{
     $serverName = "technetdb.czo9ia0scsp3.ap-northeast-2.rds.amazonaws.com";
     $connectionOptions = array(
         "database" => "technetdbver2",
@@ -8,62 +8,35 @@
         "pwd" => "technet4111",
         "TrustServerCertificate" => "yes",
     );
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
-     if($conn == false)
-         die(FormatErrors(sqlsrv_errors()));
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if($conn == false)
+        die(FormatErrors(sqlsrv_errors()));
 
-     return $conn;
- }
-?>
+    return $conn;
+}
 
-      
-        <head>
-        <meta charset="utf-8">
-        <title>74스케쥴</title>
-        <style>
-                table {
-                width: 30%;
-                border: 1px solid #444444;
-                border-collapse: collapse;
-            }
-            th, td {
-                border: 1px solid #444444;
-                padding: 10px;
-            }
-        </style>
-      </head>
-    
-    
-    <body>
-    <h1> 74line 스케쥴 : </h1>
-    
-    <?php
-      $conn = OpenConnection();
-      
-          echo '<table class="text-center"><tr>' .
-          '<th>SN</th><th>품번</th><th>수량</th>' .
-          '</tr>';
-    
-        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-            echo '<tr><td>' . $row['SN']. '</td>' .
-                '<td>' . $row['품번']. '</td>' .
-                '<td>' . $row['수량'].'</td></tr>';
-        }
-            echo '</table>';
-    
-    
-        sqlsrv_free_stmt($stmt);
-        sqlsrv_close($conn);
-    
-        function formatErrors($errors)
+function ReadData()
+    {
+        try
         {
-            // Display errors
-            echo "<h1>SQL Error:</h1>";
-            echo "Error information: <br/>";
-            foreach ($errors as $error) {
-                echo "SQLSTATE: ". $error['SQLSTATE'] . "<br/>";
-                echo "Code: ". $error['code'] . "<br/>";
-                echo "Message: ". $error['message'] . "<br/>";
+            $conn = OpenConnection();
+            $tsql = "exec phptest";
+            $getProducts = sqlsrv_query($conn, $tsql);
+            if ($getProducts == FALSE)
+                die(FormatErrors(sqlsrv_errors()));
+            $productCount = 0;
+            while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+            {
+                echo($row['품번']);
+                echo("<br/>");
+                $productCount++;
             }
+            sqlsrv_free_stmt($getProducts);
+            sqlsrv_close($conn);
         }
-    ?>
+        catch(Exception $e)
+        {
+            echo("Error!");
+        }
+    }
+?>
