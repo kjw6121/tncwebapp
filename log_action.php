323c6803a2d12909a3ee274453ee98c85531ff0e
kjw6121 <technet4111@gmail.com>
2022-07-21 10:45:09 +0900

    hh

--- log_action.php
+++ log_action.php
@@ -2,12 +2,12 @@
  function OpenConnection()
  {
     $serverName = "technetdb.czo9ia0scsp3.ap-northeast-2.rds.amazonaws.com";
-$connectionInfo = arrayarray(
-    "database" => "technetdbver2",
-    "uid" => "kim_jinwoo",
-    "pwd" => "technet4111",
-    "TrustServerCertificate" => "yes",
-);
+    $connectionOptions = array(
+        "database" => "technetdbver2",
+        "uid" => "kim_jinwoo",
+        "pwd" => "technet4111",
+        "TrustServerCertificate" => "yes",
+    );
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
      if($conn == false)
          die(FormatErrors(sqlsrv_errors()));
@@ -22,7 +22,7 @@ function ReadData()
     try
     {
         $conn = OpenConnection();
-        $tsql = "SSELECT 성명 FROM 담당자 WHERE 사용자ID='$username' AND pw='$password'";
+        $tsql = "SELECT 성명 FROM 담당자 WHERE 사용자ID='$username' AND pw='$password'";
         $getProducts = sqlsrv_query($conn, $tsql);
         if ($getProducts == FALSE)
             die(FormatErrors(sqlsrv_errors()));
@@ -41,20 +41,4 @@ function ReadData()
         echo("Error!");
     }
 }
-?>
-
-<?php
-//$username = $_REQUEST['uNm'];
-//$password  = $_REQUEST['uPw'];
-//$tsql = "SELECT * FROM 담당자 WHERE 사용자ID='$username' AND pw='$password'";
-//$stmt = sqlsrv_query( $conn, $tsql);
-//if(!sqlsrv_fetch($stmt)){
-   // $_SESSION['valid_user'] = true;
-   // $_SESSION['uNm'] = $username;
-   // header('Location: index.php');
-   // die();
-//}else{
-  //  header('Location: error.html');
-   // die();
-//}
 ?>
