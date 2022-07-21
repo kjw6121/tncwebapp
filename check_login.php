bb632f374d2889db4f8a5651c018a7001cb9fc64
kjw6121 <technet4111@gmail.com>
2022-07-21 12:11:23 +0900

    rty

--- check_login.php
+++ check_login.php
@@ -29,9 +29,10 @@
       
       $tsql = "SELECT 사용자ID FROM 담당자 WHERE 사용자ID = '$username' AND pw = '$userpass'";
       $stmt = sqlsrv_query($conn, $tsql);
+      $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
         
       //결과가 존재하면 세션 생성
-      if ($stmt != null) {
+      if ($row != null) {
          $_SESSION['username'] = $row['사용자ID'];
          $_SESSION['name'] = $row['사용자ID'];
          echo "<script>location.replace('index.php');</script>";
@@ -39,7 +40,7 @@
       }
       
       //결과가 존재하지 않으면 로그인 실패
-      if($stmt == null){
+      if($row == null){
          echo "<script>alert('Invalid username or password')</script>";
          echo "<script>location.replace('login.php');</script>";
          exit;
