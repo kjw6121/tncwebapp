c17065a2ac218d47f77f1f77d4f8206c733e2235
kjw6121 <technet4111@gmail.com>
2022-07-21 12:10:13 +0900

    qwe

--- check_login.php
+++ check_login.php
@@ -27,13 +27,13 @@
       $username = $_POST['id'];
       $userpass = $_POST['pw'];
       
-      $tsql = "SELECT 사용자ID, 성명 FROM 담당자 WHERE 사용자ID = '$username' AND pw = '$userpass'";
+      $tsql = "SELECT 사용자ID FROM 담당자 WHERE 사용자ID = '$username' AND pw = '$userpass'";
       $stmt = sqlsrv_query($conn, $tsql);
         
       //결과가 존재하면 세션 생성
       if ($stmt != null) {
          $_SESSION['username'] = $row['사용자ID'];
-         $_SESSION['name'] = $row['성명'];
+         $_SESSION['name'] = $row['사용자ID'];
          echo "<script>location.replace('index.php');</script>";
          exit;
       }
