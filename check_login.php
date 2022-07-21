935c791e441bdb1ad5f99fd39b0a843a78aa6a11
kjw6121 <technet4111@gmail.com>
2022-07-21 11:55:37 +0900

    newlog

---
+++ check_login.php
@@ -0,0 +1,65 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title></title>
+</head>
+<body>
+   <?php
+    $serverName = "technetdb.czo9ia0scsp3.ap-northeast-2.rds.amazonaws.com";
+    $connectionOptions = array(
+        "database" => "technetdbver2",
+        "uid" => "kim_jinwoo",
+        "pwd" => "technet4111",
+        "TrustServerCertificate" => "yes",
+    );
+        
+    // Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if ($conn === false) {
+        die(formatErrors(sqlsrv_errors()));
+    }
+
+    // Select Query
+    $tsql = "SELECT SN, 품번, 수량  FROM BHsch74";
+
+
+    // Executes the query
+    $stmt = sqlsrv_query($conn, $tsql);
+
+    // Error handling
+    if ($stmt === false) {
+        die(formatErrors(sqlsrv_errors()));
+    }
+   
+      //login.php에서 입력받은 id, password
+      $username = $_POST['id'];
+      $userpass = $_POST['pw'];
+      
+      $tsql = "SELECT 사용자ID, 성명 FROM 담당자 WHERE 사용자ID = '$username' AND pw = '$userpass'";
+      $stmt = sqlsrv_query($conn, $tsql);
+      $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+      
+      //결과가 존재하면 세션 생성
+      if ($row != null) {
+         $_SESSION['username'] = $row['사용자ID'];
+         $_SESSION['name'] = $row['성명'];
+         echo "<script>location.replace('index.php');</script>";
+         exit;
+      }
+      
+      //결과가 존재하지 않으면 로그인 실패
+      if($row == null){
+         echo "<script>alert('Invalid username or password')</script>";
+         echo "<script>location.replace('login.php');</script>";
+         exit;
+      }
+
+      sqlsrv_free_stmt($stmt);
+      sqlsrv_close($conn);
+
+?>
+   </body>
